2fdef4833b3dc867d4a84f2f77f6aa5a4858cf93
Seoghyun Kim <67981651+seoghyunkim@users.noreply.github.com>
2021-08-01 19:16:28 -0400

    edited

--- analysis_GLMM.R
+++ analysis_GLMM.R
@@ -38,26 +38,24 @@ fish_dat <- fish_dat %>%
 #'- Common shiner data (110 watersheds)
     CMS_dat  <- fish_dat
 
-  
 
 ##### GLMM using package spaMM #####  
 #'- Matern correlation function was included to account for spatial autocorrelation
 
-
 #'## Common Shiner Model
 
-#'- Full model
-  CMS_full <- fitme(cbind(CMS_num, SiteNUM-CMS_num)~ scale(HHC_prop) + scale(CRC_prop) + 
+#'- Model
+  CMS_mod <- fitme(cbind(CMS_num, SiteNUM-CMS_num)~ scale(HHC_prop) + scale(CRC_prop) + 
                    scale(Area) +  scale(Per_Agriculture) + scale(Elevation) + scale(resid_Temp) + Matern(1|Lat + Lon), 
                    family= binomial, data= CMS_dat, method="ML")
   
-  summary(CMS_full)
+  summary(CMS_mod)
   
 #'- Model validation - Pearson Correlation
-  cor.test(predict(CMS_full), CMS_dat$CMS_prop)
+  cor.test(predict(CMS_mod), CMS_dat$CMS_prop)
   
   cms_val <- ggplot() + 
-    geom_point(aes(as.vector(predict(CMS_full)), CMS_dat$CMS_prop)) + 
+    geom_point(aes(as.vector(predict(CMS_mod)), CMS_dat$CMS_prop)) + 
     scale_x_continuous("Predicted value", breaks = seq(0,1,0.2)) +
     scale_y_continuous("Observed value", breaks = seq(0,1,0.2)) +
     labs(title= "Common Shiner") +
@@ -74,35 +72,35 @@ fish_dat <- fish_dat %>%
     theme(panel.background = element_rect(fill = "white"), legend.position=c(0.10,0.80))
 
 #'- Extract 95% CI  
-  CMS_full_coef <- as.data.frame(summary(CMS_full)$beta_table)
-  row0 <- row.names(CMS_full_coef) %in% c('(Intercept)')
-  row1 <- row.names(CMS_full_coef) %in% c('scale(HHC_prop)')
-  row2 <- row.names(CMS_full_coef) %in% c('scale(CRC_prop)')
-  row3 <- row.names(CMS_full_coef) %in% c('scale(Area)')
-  row4 <- row.names(CMS_full_coef) %in% c('scale(Per_Agriculture)')
-  row5 <- row.names(CMS_full_coef) %in% c('scale(Elevation)')
-  row6 <- row.names(CMS_full_coef) %in% c('scale(resid_Temp)')
+  CMS_mod_coef <- as.data.frame(summary(CMS_mod)$beta_table)
+  row0 <- row.names(CMS_mod_coef) %in% c('(Intercept)')
+  row1 <- row.names(CMS_mod_coef) %in% c('scale(HHC_prop)')
+  row2 <- row.names(CMS_mod_coef) %in% c('scale(CRC_prop)')
+  row3 <- row.names(CMS_mod_coef) %in% c('scale(Area)')
+  row4 <- row.names(CMS_mod_coef) %in% c('scale(Per_Agriculture)')
+  row5 <- row.names(CMS_mod_coef) %in% c('scale(Elevation)')
+  row6 <- row.names(CMS_mod_coef) %in% c('scale(resid_Temp)')
   
-  lower0 <- CMS_full_coef[row0,'Estimate'] - 1.96*CMS_full_coef[row0, 'Cond. SE']
-  upper0 <- CMS_full_coef[row0,'Estimate'] + 1.96*CMS_full_coef[row0, 'Cond. SE']
+  lower0 <- CMS_mod_coef[row0,'Estimate'] - 1.96*CMS_mod_coef[row0, 'Cond. SE']
+  upper0 <- CMS_mod_coef[row0,'Estimate'] + 1.96*CMS_mod_coef[row0, 'Cond. SE']
   
-  lower1 <- CMS_full_coef[row1,'Estimate'] - 1.96*CMS_full_coef[row1, 'Cond. SE']
-  upper1 <- CMS_full_coef[row1,'Estimate'] + 1.96*CMS_full_coef[row1, 'Cond. SE']
+  lower1 <- CMS_mod_coef[row1,'Estimate'] - 1.96*CMS_mod_coef[row1, 'Cond. SE']
+  upper1 <- CMS_mod_coef[row1,'Estimate'] + 1.96*CMS_mod_coef[row1, 'Cond. SE']
   
-  lower2 <- CMS_full_coef[row2,'Estimate'] - 1.96*CMS_full_coef[row2, 'Cond. SE']
-  upper2 <- CMS_full_coef[row2,'Estimate'] + 1.96*CMS_full_coef[row2, 'Cond. SE']
+  lower2 <- CMS_mod_coef[row2,'Estimate'] - 1.96*CMS_mod_coef[row2, 'Cond. SE']
+  upper2 <- CMS_mod_coef[row2,'Estimate'] + 1.96*CMS_mod_coef[row2, 'Cond. SE']
   
-  lower3 <- CMS_full_coef[row3,'Estimate'] - 1.96*CMS_full_coef[row3, 'Cond. SE']
-  upper3 <- CMS_full_coef[row3,'Estimate'] + 1.96*CMS_full_coef[row3, 'Cond. SE']
+  lower3 <- CMS_mod_coef[row3,'Estimate'] - 1.96*CMS_mod_coef[row3, 'Cond. SE']
+  upper3 <- CMS_mod_coef[row3,'Estimate'] + 1.96*CMS_mod_coef[row3, 'Cond. SE']
 
-  lower4 <- CMS_full_coef[row4,'Estimate'] - 1.96*CMS_full_coef[row4, 'Cond. SE']
-  upper4 <- CMS_full_coef[row4,'Estimate'] + 1.96*CMS_full_coef[row4, 'Cond. SE']
+  lower4 <- CMS_mod_coef[row4,'Estimate'] - 1.96*CMS_mod_coef[row4, 'Cond. SE']
+  upper4 <- CMS_mod_coef[row4,'Estimate'] + 1.96*CMS_mod_coef[row4, 'Cond. SE']
   
-  lower5 <- CMS_full_coef[row5,'Estimate'] - 1.96*CMS_full_coef[row5, 'Cond. SE']
-  upper5 <- CMS_full_coef[row5,'Estimate'] + 1.96*CMS_full_coef[row5, 'Cond. SE']
+  lower5 <- CMS_mod_coef[row5,'Estimate'] - 1.96*CMS_mod_coef[row5, 'Cond. SE']
+  upper5 <- CMS_mod_coef[row5,'Estimate'] + 1.96*CMS_mod_coef[row5, 'Cond. SE']
   
-  lower6 <- CMS_full_coef[row6,'Estimate'] - 1.96*CMS_full_coef[row6, 'Cond. SE']
-  upper6 <- CMS_full_coef[row6,'Estimate'] + 1.96*CMS_full_coef[row6, 'Cond. SE']
+  lower6 <- CMS_mod_coef[row6,'Estimate'] - 1.96*CMS_mod_coef[row6, 'Cond. SE']
+  upper6 <- CMS_mod_coef[row6,'Estimate'] + 1.96*CMS_mod_coef[row6, 'Cond. SE']
   
   cms_low <- rbind(lower0, lower1, lower2, lower3 ,lower4, lower5, lower6)
   cms_upp <- rbind(upper0, upper1, upper2, upper3, upper4, upper5, upper6)
@@ -140,13 +138,10 @@ fish_dat <- fish_dat %>%
   
   cms_CI_plot
   
-
-
 #'-  Regression plot
-  CMS_HHC_efct <- plot_effects(CMS_full,focal_var="HHC_prop", rgb.args = col2rgb("black"))
-  CMS_CRC_efct <- plot_effects(CMS_full,focal_var="CRC_prop", rgb.args = col2rgb("black"))
-  CMS_elv_efct <- plot_effects(CMS_full,focal_var="Elevation", rgb.args = col2rgb("black"))
-  
+  CMS_HHC_efct <- plot_effects(CMS_mod,focal_var="HHC_prop", rgb.args = col2rgb("black"))
+  CMS_CRC_efct <- plot_effects(CMS_mod,focal_var="CRC_prop", rgb.args = col2rgb("black"))
+  CMS_elv_efct <- plot_effects(CMS_mod,focal_var="Elevation", rgb.args = col2rgb("black"))
   
 CMS_HHC_fig <- ggplot() +
   geom_ribbon(data = CMS_HHC_efct, aes(x = focal_var, ymin = low, ymax = up), alpha = 0.2) +
@@ -258,17 +253,17 @@ ggarrange(CMS_HHC_fig, CMS_CRC_fig, CMS_area_fig, CMS_agri_fig,  CMS_elv_fig, CM
 ##### SRD model #####
 #'## Southern Redbelly Dace Model
 
-#'- Full model
-  SRD_full <- fitme(cbind(SRD_num, SiteNUM-SRD_num)~ scale(HHC_prop) + scale(CRC_prop) + scale(CSR_prop) + 
+#'- mod model
+  SRD_mod <- fitme(cbind(SRD_num, SiteNUM-SRD_num)~ scale(HHC_prop) + scale(CRC_prop) + scale(CSR_prop) + 
                    scale(Area) +  scale(Per_Agriculture) + scale(Elevation) + scale(resid_Temp) + Matern(1|Lat + Lon),
                    family= binomial, data= SRD_dat, method="ML")
-  summary(SRD_full)
+  summary(SRD_mod)
   
 #'- Model validation - Pearson Correlation
-  cor.test(predict(SRD_full), SRD_dat$SRD_prop)
+  cor.test(predict(SRD_mod), SRD_dat$SRD_prop)
   
   srd_val <-ggplot() + 
-    geom_point(aes(as.vector(predict(SRD_full)), SRD_dat$SRD_prop)) + 
+    geom_point(aes(as.vector(predict(SRD_mod)), SRD_dat$SRD_prop)) + 
     scale_x_continuous("Predicted value", limits =  c(0,1)) +
     scale_y_continuous("Observed value", limits =  c(0,1)) +
     labs(title= "Southern redbelly dace") +
@@ -291,39 +286,39 @@ ggarrange(CMS_HHC_fig, CMS_CRC_fig, CMS_area_fig, CMS_agri_fig,  CMS_elv_fig, CM
             ncol = 1, nrow = 2)
 
 #'- Extract 95% CI  
-  SRD_full_coef <- as.data.frame(summary(SRD_full)$beta_table)
-  row0 <- row.names(SRD_full_coef) %in% c('(Intercept)')
-  row1 <- row.names(SRD_full_coef) %in% c('scale(HHC_prop)')
-  row2 <- row.names(SRD_full_coef) %in% c('scale(CRC_prop)')
-  row3 <- row.names(SRD_full_coef) %in% c('scale(CSR_prop)')
-  row4 <- row.names(SRD_full_coef) %in% c('scale(Area)')
-  row5 <- row.names(SRD_full_coef) %in% c('scale(Per_Agriculture)')
-  row6 <- row.names(SRD_full_coef) %in% c('scale(Elevation)')
-  row7 <- row.names(SRD_full_coef) %in% c('scale(resid_Temp)')
+  SRD_mod_coef <- as.data.frame(summary(SRD_mod)$beta_table)
+  row0 <- row.names(SRD_mod_coef) %in% c('(Intercept)')
+  row1 <- row.names(SRD_mod_coef) %in% c('scale(HHC_prop)')
+  row2 <- row.names(SRD_mod_coef) %in% c('scale(CRC_prop)')
+  row3 <- row.names(SRD_mod_coef) %in% c('scale(CSR_prop)')
+  row4 <- row.names(SRD_mod_coef) %in% c('scale(Area)')
+  row5 <- row.names(SRD_mod_coef) %in% c('scale(Per_Agriculture)')
+  row6 <- row.names(SRD_mod_coef) %in% c('scale(Elevation)')
+  row7 <- row.names(SRD_mod_coef) %in% c('scale(resid_Temp)')
   
-  lower0 <- SRD_full_coef[row0,'Estimate'] - 1.96*SRD_full_coef[row0, 'Cond. SE']
-  upper0 <- SRD_full_coef[row0,'Estimate'] + 1.96*SRD_full_coef[row0, 'Cond. SE']
+  lower0 <- SRD_mod_coef[row0,'Estimate'] - 1.96*SRD_mod_coef[row0, 'Cond. SE']
+  upper0 <- SRD_mod_coef[row0,'Estimate'] + 1.96*SRD_mod_coef[row0, 'Cond. SE']
   
-  lower1 <- SRD_full_coef[row1,'Estimate'] - 1.96*SRD_full_coef[row1, 'Cond. SE']
-  upper1 <- SRD_full_coef[row1,'Estimate'] + 1.96*SRD_full_coef[row1, 'Cond. SE']
+  lower1 <- SRD_mod_coef[row1,'Estimate'] - 1.96*SRD_mod_coef[row1, 'Cond. SE']
+  upper1 <- SRD_mod_coef[row1,'Estimate'] + 1.96*SRD_mod_coef[row1, 'Cond. SE']
   
-  lower2 <- SRD_full_coef[row2,'Estimate'] - 1.96*SRD_full_coef[row2, 'Cond. SE']
-  upper2 <- SRD_full_coef[row2,'Estimate'] + 1.96*SRD_full_coef[row2, 'Cond. SE']
+  lower2 <- SRD_mod_coef[row2,'Estimate'] - 1.96*SRD_mod_coef[row2, 'Cond. SE']
+  upper2 <- SRD_mod_coef[row2,'Estimate'] + 1.96*SRD_mod_coef[row2, 'Cond. SE']
   
-  lower3 <- SRD_full_coef[row3,'Estimate'] - 1.96*SRD_full_coef[row3, 'Cond. SE']
-  upper3 <- SRD_full_coef[row3,'Estimate'] + 1.96*SRD_full_coef[row3, 'Cond. SE']
+  lower3 <- SRD_mod_coef[row3,'Estimate'] - 1.96*SRD_mod_coef[row3, 'Cond. SE']
+  upper3 <- SRD_mod_coef[row3,'Estimate'] + 1.96*SRD_mod_coef[row3, 'Cond. SE']
   
-  lower4 <- SRD_full_coef[row4,'Estimate'] - 1.96*SRD_full_coef[row4, 'Cond. SE']
-  upper4 <- SRD_full_coef[row4,'Estimate'] + 1.96*SRD_full_coef[row4, 'Cond. SE']
+  lower4 <- SRD_mod_coef[row4,'Estimate'] - 1.96*SRD_mod_coef[row4, 'Cond. SE']
+  upper4 <- SRD_mod_coef[row4,'Estimate'] + 1.96*SRD_mod_coef[row4, 'Cond. SE']
   
-  lower5 <- SRD_full_coef[row5,'Estimate'] - 1.96*SRD_full_coef[row5, 'Cond. SE']
-  upper5 <- SRD_full_coef[row5,'Estimate'] + 1.96*SRD_full_coef[row5, 'Cond. SE']
+  lower5 <- SRD_mod_coef[row5,'Estimate'] - 1.96*SRD_mod_coef[row5, 'Cond. SE']
+  upper5 <- SRD_mod_coef[row5,'Estimate'] + 1.96*SRD_mod_coef[row5, 'Cond. SE']
   
-  lower6 <- SRD_full_coef[row6,'Estimate'] - 1.96*SRD_full_coef[row6, 'Cond. SE']
-  upper6 <- SRD_full_coef[row6,'Estimate'] + 1.96*SRD_full_coef[row6, 'Cond. SE']
+  lower6 <- SRD_mod_coef[row6,'Estimate'] - 1.96*SRD_mod_coef[row6, 'Cond. SE']
+  upper6 <- SRD_mod_coef[row6,'Estimate'] + 1.96*SRD_mod_coef[row6, 'Cond. SE']
   
-  lower7 <- SRD_full_coef[row7,'Estimate'] - 1.96*SRD_full_coef[row7, 'Cond. SE']
-  upper7 <- SRD_full_coef[row7,'Estimate'] + 1.96*SRD_full_coef[row7, 'Cond. SE']
+  lower7 <- SRD_mod_coef[row7,'Estimate'] - 1.96*SRD_mod_coef[row7, 'Cond. SE']
+  upper7 <- SRD_mod_coef[row7,'Estimate'] + 1.96*SRD_mod_coef[row7, 'Cond. SE']
   
   srd_low <- rbind(lower0, lower1, lower2, lower3 ,lower4, lower5, lower6, lower7)
   srd_upp <- rbind(upper0, upper1, upper2, upper3, upper4, upper5, upper6, upper7)
@@ -363,8 +358,8 @@ ggarrange(CMS_HHC_fig, CMS_CRC_fig, CMS_area_fig, CMS_agri_fig,  CMS_elv_fig, CM
 
   
 #'- Regression plot
-  SRD_CSR_efct <- plot_effects(SRD_full,focal_var="CSR_prop", rgb.args = col2rgb("black"))
-  SRD_agri_efct <- plot_effects(SRD_full,focal_var="Per_Agriculture", rgb.args = col2rgb("black"))
+  SRD_CSR_efct <- plot_effects(SRD_mod,focal_var="CSR_prop", rgb.args = col2rgb("black"))
+  SRD_agri_efct <- plot_effects(SRD_mod,focal_var="Per_Agriculture", rgb.args = col2rgb("black"))
 
   SRD_HHC_fig <- ggplot() +
     geom_point(data = SRD_dat, aes(x = HHC_prop, y = SRD_prop), shape=21, colour="black") +
